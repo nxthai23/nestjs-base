be4e8a907d675adabb26c04e60a8daa53a2edc13
thai <nxthai23@gmail.com>
2023-12-15 11:26:12 +0700

    add jwt middleware

--- src/guard/jwt.auth.guard.ts
+++ src/guard/jwt.auth.guard.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
+/**
+ * This guard is used to protect routes that require a valid JWT token
+ * @see https://docs.nestjs.com/guards
+ */
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,23 +3,22 @@ import {
   Get,
   Body,
   Patch,
-  Param,
   Delete,
   BadRequestException,
   UseGuards,
   UseInterceptors,
   SerializeOptions,
   Query,
+  Req,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { JwtAuthGuard } from 'src/guard/jwt.auth.guard';
-import { ParseObjectIdPipe } from 'src/pipe/objectId.pipe';
 import { UserSerialize } from './interceptor/user.interceptor';
 
 // @TODO: add admin validation later for this controller
 
-@Controller('user')
+@Controller('users')
 @UseGuards(JwtAuthGuard)
 @SerializeOptions({
   excludePrefixes: ['password'],
@@ -29,7 +28,7 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
-  fetch(@Query('query') query: object) {
+  fetch(@Query('query') query?: object) {
     try {
       if (!query) {
         return this.userService.findAll();
@@ -40,32 +39,32 @@ export class UserController {
     }
   }
 
-  @Get(':id')
-  findOne(@Param('id', ParseObjectIdPipe) id: string) {
+  @Get('/me')
+  async findOne(@Req() req: Request) {
     try {
-      return this.userService.findOne(id);
+      const userId = req['userId'];
+      return await this.userService.findOne(userId);
     } catch (err) {
       console.log(err.message);
       return new BadRequestException(err.message);
     }
   }
 
-  @Patch(':id')
-  update(
-    @Param('id', ParseObjectIdPipe) id: string,
-    @Body() updateUserDto: UpdateUserDto,
-  ) {
+  @Patch('/me')
+  update(@Req() req: Request, @Body() updateUserDto: UpdateUserDto) {
     try {
-      return this.userService.update(id, updateUserDto);
+      const userId = req['userId'];
+      return this.userService.update(userId, updateUserDto);
     } catch (err) {
       return new BadRequestException(err.message);
     }
   }
 
-  @Delete(':id')
-  delete(@Param('id', ParseObjectIdPipe) id: string) {
+  @Delete('/me')
+  delete(@Req() req: Request) {
     try {
-      return this.userService.delete(id);
+      const userId = req['userId'];
+      return this.userService.delete(userId);
     } catch (err) {
       return new BadRequestException(err.message);
     }
