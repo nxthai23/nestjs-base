7b72ac3175acd4965e2681dd2748bca92000e58d
thai <nxthai23@gmail.com>
2026-08-21 15:06:44 +0700

    feat: enforce CASL policies on UserController routes

--- src/api/user/user.module.ts
+++ src/api/user/user.module.ts
@@ -7,9 +7,10 @@ import { JwtService } from '@nestjs/jwt';
 import { JwtStrategy } from '@/api/auth/strategies/jwt';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { RoleModule } from '@api/role/role.module';
+import { CaslModule } from '@core/casl/casl.module';
 
 @Module({
-  imports: [MikroOrmModule.forFeature([User]), RoleModule],
+  imports: [MikroOrmModule.forFeature([User]), RoleModule, CaslModule],
   controllers: [UserController],
   providers: [UserService, JwtService, JwtStrategy],
   exports: [UserService],
